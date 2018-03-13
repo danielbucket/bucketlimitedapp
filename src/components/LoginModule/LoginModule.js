2d0145c4c05dde325b1f39620a63bb96a0c5e5cb
Daniel Bucket <danieljbucket@gmail.com>
2018-03-12 19:31:14 -0600

    app.yaml file is for google cloud platform. updated LoginModule display functionality

--- src/components/LoginModule/LoginModule.js
+++ src/components/LoginModule/LoginModule.js
@@ -1,5 +1,5 @@
 import React, { Component, Proptypes } from 'react';
-import style from './css/LoginModuleRoot.css';
+import './css/LoginModuleRoot.css';
 
 export default class LoginModule extends Component {
 	constructor() {
@@ -8,7 +8,8 @@ export default class LoginModule extends Component {
 			userName: "",
 			email: "",
 			password: "",
-			active: false
+			active: false,
+			disabled: false
 		}
 
 		this.handleChange = this.handleChange.bind(this);
@@ -16,8 +17,8 @@ export default class LoginModule extends Component {
 	}
 
 	handleChange(e) {
-		const input = e.target.value
-		const stateKey = e.target.id
+		const input = e.target.value;
+		const stateKey = e.target.id;
 
 		this.setState({ [stateKey]:input })
 	}
@@ -28,47 +29,47 @@ export default class LoginModule extends Component {
 
 	toggleClass() {
 		if (!this.state.active) {
+			this.setState({ disabled:true })
 			this.setState({ active:true })
-		} else {
-			this.setState({ active:false })	
 		}
 	}
 
 
 	render() {
-		const { userName,email,password } = this.state;
+		const { userName,email,password,disabled } = this.state;
 		let status = this.state.active ? "active" : "inactive";
 
 		return (
 			<section className="login_box">
-				<h3 className="login_text"
-						onClick={e => this.toggleClass() }>
+				<button className="login_text"
+								disabled={ disabled }
+								onClick={() => this.toggleClass() }>
 							Log In / Sign Up
-						</h3>
+				</button>
 				<div className={ status }>
 					<input id="userName"
 								 type="text"
-								 className="username_text loginHidden"
+								 className="username_text"
 								 placeholder="user name"
 								 value={ userName }
 								 onChange = {e => this.handleChange(e)} />
 
 					<input id="email"
 								 type="email"
-								 className="email_text loginHidden"
+								 className="email_text"
 								 placeholder="email"
 								 value={ email }
 								 onChange = {e => this.handleChange(e)} />
 
 					<input id="password"
 								 type="text"
-								 className="password_text loginHidden"
+								 className="password_text"
 								 placeholder="password"
 								 value={ password }
 								 onChange = {e => this.handleChange(e)} />
 
 					<button id="submitBtn"
-									className="submitBtn loginHidden"
+									className="submitBtn"
 									onClick={() => this.submitLogin()}>
 									Submit</button>
 				</div>
