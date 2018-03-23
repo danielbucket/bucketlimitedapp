be8df593f0aa9a8093e7f61b5f4c0ab2494b6a29
Daniel Bucket <danieljbucket@gmail.com>
2018-03-23 00:18:01 -0600

    well, I strayed over to the PageUnderContruction branch for a minute, but then ended back back on master. Added some widget css code as well as addres button that opens up the github project repo page in a new tab. Also added in bitcoin and etherium wallet addresses cause why not? Anythings helps.

--- src/components/PageUnderConstruction/PageUnderConstruction.js
+++ src/components/PageUnderConstruction/PageUnderConstruction.js
@@ -11,8 +11,10 @@ export default class PageUnderConstruction extends Component {
 		this.state = {
 			messages:[],
 			gitHubProfile: "danielbucket",
-			gitHubRepo: "bucketlimitedapp"
-			// gitHubProile and gitHubRepo will be passed in as props
+			projectRepo: "bucketlimitedapp",
+			BTC_Address: "1DyXVZpmGRVPvNvsmWYimHeGzKEqxNaZT",
+			ETH_Address: "0xc097Bbb6CF49d70DAD90149Fc4d932Fc730F1080"
+			// gitHubProile and projectRepo will be passed in as props
 			// in future iterations of this app thus keeping
 			// PageUnderConstructuction.js modular
 		}
@@ -20,11 +22,12 @@ export default class PageUnderConstruction extends Component {
 		this.fetchGitHub = fetchGitHub.bind(this)
 		this.stateSet = this.stateSet.bind(this)
 		this.GitHubCommitMessage = GitHubCommitMessage.bind(this)
+		this.openNewBrowserTab = this.openNewBrowserTab.bind(this)
 	}
 
 	componentWillMount() {
 		let profile = this.state.gitHubProfile
-		let repo = this.state.gitHubRepo
+		let repo = this.state.projectRepo
 		const gitHubFetch = this.fetchGitHub(profile, repo, this.stateSet);
 
 
@@ -44,12 +47,17 @@ export default class PageUnderConstruction extends Component {
 	}
 
 
+	openNewBrowserTab() {
+		window.open(`https://github.com/${this.state.gitHubProfile}/${this.state.projectRepo}`)
+	}
+
 
 	render() {
 		const btnImage = this.props.btnImage || "X";
 		const { closeModule } = this.props;
 		const popUpBarrier = this.props.popUpBool ? "popUpBarrier" : "popUpBarrier popUpInactive";
-		const messagesArr = this.state.messages;
+		const { BTC_Address, ETH_Address } = this.state;
+		const messagesArr = this.state.messages
 
 		return (
 			<div className={ popUpBarrier }>
@@ -61,10 +69,23 @@ export default class PageUnderConstruction extends Component {
 						<div className="statementBox">
 							<p>Page Under Construction</p>
 							<p>This is my personal webpage and is still very much in the works.</p>
-							<p>Please forgive the mess</p>
+							<p>Please forgive the mess.</p>
 						</div>
 						<GitHubCommitMessage messagesArr={ messagesArr }
 																 gitHubProfile={ this.state.gitHubProfile }/>
+						<button className="gitHubReopLink"
+										onClick={ () => this.openNewBrowserTab() }>
+							Check out my GitHub repository.
+						</button>
+
+						<div className="cryptoCoinAddressContainer">
+							<div className="cryptoCoinAddress">
+								BTC: { BTC_Address }
+							</div>
+							<div className="cryptoCoinAddress">
+								ETH: { ETH_Address }
+							</div>
+						</div>
 				</div>
 			</div>
 		)
--- src/components/PageUnderConstruction/gitHubCommitMessage.js
+++ src/components/PageUnderConstruction/gitHubCommitMessage.js
@@ -7,20 +7,30 @@ const GitHubCommitMessage = ({ messagesArr }) => {
 		return (
 			<div className="commitCard"
 					 key={ i.timeStamp }>
-					 #{ commitNum }
-				<div className="commitTimeStamp">
-					{ i.timeStamp }
+				<div className="verticleTextAlignWrapper">
+					<p className="commitNum">
+						#{ commitNum }
+					</p>
 				</div>
-				<div className="commitMessage">
-					{ i.message }
+
+				<div className="commitDetails">
+					<div className="commitTimeStamp">
+						{ i.timeStamp }
+					</div>
+					<div className="commitMessage">
+						{ i.message }
+					</div>
 				</div>
 			</div>
 		)
 	})
 
 	return (
-		<div  className="gitHubWidgetContainer">
-			<h5>GitHub Commits Widget</h5>
+		<div className="gitHubWidgetContainer">
+			<div className="verticleTextAlignWrapper widgetTitle">
+				<p>GitHub Commits Widget</p>
+			</div>
+
 			<div className="commitCardContainer">
 				{ cardData }
 			</div>
